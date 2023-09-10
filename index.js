42eadc6f11d4f0b6f11a4866fa0a9393783cfd45
OlgaKuvich <olga.kuvichinskaya@mail.ru>
2023-09-10 23:35:29 +0300

    Added JS

---
+++ index.js
@@ -0,0 +1,86 @@
+
+//Задание 1
+let a = 10;
+alert(a);
+
+a = 20;
+alert(a);
+
+//Задание 2
+let iPhone = 2007;
+alert(iPhone);
+
+//Задание 3
+let author = 'Брендан Айк';
+alert(author);
+
+//Задание 4
+let c = 10;
+let d = 2;
+alert(c + d);
+alert(c - d);
+alert(c * d);
+alert(c / d);
+
+//Задание 5
+let result = 2**5;
+alert(result);
+
+//Задание 6
+let i = 9;
+let b = 2;
+alert(i % b);
+
+//Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+//Задание 8
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+//Задание 9.0
+let user = {     
+    name: "Ольга",  
+    age: 44,
+    isAdmin: true        
+  };
+  alert(user .name);
+  alert(user .age);
+  alert(user .isAdmin);
+
+//Задание 9.1
+user["city of residence"] = true;
+alert(["city of residence"]);
+
+//Задание 9.2
+user .age = 18;
+alert(user .age);
+
+//Задание 9.3
+delete user["city of residence"];
+
+//Задание 9.4
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(info);
+
+//Задание 10
+user = prompt("Как ваше имя?");
+alert(`Привет ${user}!`);
+
+
+
+
+
+
+
+
+
+
+
